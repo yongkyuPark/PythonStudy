cb5dde162a6b08806b6157e1c0e26b9530b02882
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 01:15:45 +0900

    feat : 숫자 처리 함수

---
+++ python-basic/practice2_3.py
@@ -0,0 +1,11 @@
+print(abs(-5)) # 절대값 구하기 = 5
+print(pow(4, 2)) # 4의 제곱 = 16
+print(max(5, 12)) # 더 큰 값 구하기 = 12
+print(min(5, 12)) # 더 작은 값 구하기 = 5
+print(round(3.14)) # 반올림 = 3
+print(round(4.99)) # 5
+
+from math import *
+print(floor(4.99)) # 내림 = 4
+print(ceil(3.14)) # 올림 = 4
+print(sqrt(16)) # 제곱근 구하기 = 4
