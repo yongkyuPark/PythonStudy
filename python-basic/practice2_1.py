27a7947adc7629da906ed476f9550869f086d4b0
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 01:08:45 +0900

    feat : 연산자

---
+++ python-basic/practice2_1.py
@@ -0,0 +1,31 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2
+
+print(2**3) # 2의 세제곱 8
+print(5%3) # 나머지 구하기 2
+print(10%3) # 1
+print(5//3) # 몫 구하기 1
+print(10//3) # 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # 같다의 의미 True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # 같지 않다 True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True & -> and
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True | -> or
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
