6215c5d5de7fec783b11a5bbd4b2f177d41bae3f
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 15:11:17 +0900

    feat : 탈출문자

---
+++ python-basic/practice3_5.py
@@ -0,0 +1,21 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# 저는 "누구누구"입니다. 
+# \" \' : 문장 내에서 따옴표
+print("저는 '누구누구'입니다.")
+print('저는 "누구누구"입니다.')
+print("저는 \"누구누구\"입니다.")
+print("저는 \'누구누구\'입니다.")
+
+# \\ : 문장 내에서 \
+print("\\usr\\local\\bin\\python3")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
