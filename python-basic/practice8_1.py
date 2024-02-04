02a1de6dfbcfefedfb72a9f84116da2e57366b9d
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 21:19:03 +0900

    feat : 멤버변수

--- python-basic/practice8_1.py
+++ python-basic/practice8_1.py
@@ -9,3 +9,14 @@ class Unit:
 marine1 = Unit("마린", 40, 5)
 marine2 = Unit("마린", 40, 5)
 tank = Unit("탱크", 150, 35)
+
+# 레이스 : 공중 유닛, 비행기. 클로킹 (상대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
