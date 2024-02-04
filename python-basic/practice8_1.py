d94cd3c5549734046d3357cdfc613e4743f7f59d
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 21:38:35 +0900

    feat : 다중 상속

--- python-basic/practice8_1.py
+++ python-basic/practice8_1.py
@@ -29,3 +29,24 @@ firebat1.attack("5시")
 # 공격 2번 받는다고 가정
 firebat1.damaged(25)
 firebat1.damaged(25)
+
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+              .format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
