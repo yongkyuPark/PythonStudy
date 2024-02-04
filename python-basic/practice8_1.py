b944a302229c45ecd088c88f2e8c79a14aafe4c4
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 21:47:36 +0900

    feat : 메소드 오버라이딩

--- python-basic/practice8_1.py
+++ python-basic/practice8_1.py
@@ -1,13 +1,19 @@
 # 일반 유닛
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+              .format(self.name, location, self.speed))
 
 # 공격 유닛
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
         
     def attack(self, location):
@@ -20,17 +26,6 @@ class AttackUnit(Unit):
         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
         if self.hp <= 0:
             print("{0} : 파괴되었습니다.".format(self.name))
-# 메딕 : 의무병
-
-# 파이어뱃 : 공격 유닛, 화염방사기
-firebat1 = AttackUnit("파이어뱃", 50, 16)
-firebat1.attack("5시")
-
-# 공격 2번 받는다고 가정
-firebat1.damaged(25)
-firebat1.damaged(25)
-
-# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
 
 # 날 수 있는 기능을 가진 클래스
 class Flyable:
@@ -44,9 +39,20 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
         Flyable.__init__(self, flying_speed)
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+valture = AttackUnit("벌쳐", 80, 10, 20)
+
+# 배틀크루저 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+valture.move("11시")
+#battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
