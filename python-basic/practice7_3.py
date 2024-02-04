d4170f503965ab7323b5d39986c826cb305b2eb9
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 20:47:57 +0900

    feat : 파일입출력

---
+++ python-basic/practice7_3.py
@@ -0,0 +1,35 @@
+# score_file = open("score.txt", "w", encoding="utf8") # write
+# print("수학 : 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8") # append
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8") # read
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8") # read
+# print(score_file.readline(), end="") # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline(), end="") 
+# print(score_file.readline(), end="") 
+# print(score_file.readline(), end="") 
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8") # read
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8") # read
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
