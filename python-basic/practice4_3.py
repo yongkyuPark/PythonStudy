472b31699766ab96e126c86cfb7666d3df9cf4cc
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-04 16:02:05 +0900

    feat : 튜플

---
+++ python-basic/practice4_3.py
@@ -0,0 +1,13 @@
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# 튜플은 고정된 값을 사용 가능
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
