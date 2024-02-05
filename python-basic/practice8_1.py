8ae73ee5a73c9f9710f9589fd808458dc62ba643
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-05 20:48:57 +0900

    feat : super

--- python-basic/practice8_1.py
+++ python-basic/practice8_1.py
@@ -50,17 +50,8 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 # 건물
 class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
-        pass
+        #Unit.__init__(self, name, hp, 0)
+        super().__init__(name, hp, 0)
+        self.location = location
 
 
-# 서플라이 디폿 : 건물, 1개 건물 = 8 유닛
-supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
-
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
-
-def game_over():
-    pass
-
-game_start()
-game_over()
