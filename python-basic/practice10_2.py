b2116f511072aaaa689629ecd1db5d09b5a89e5b
Yongkyu Park <dydrbdjdsla@gmail.com>
2024-02-06 21:51:34 +0900

    feat : 패키지

---
+++ python-basic/practice10_2.py
@@ -0,0 +1,11 @@
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
